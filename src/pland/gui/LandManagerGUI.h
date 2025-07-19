2fd829be69355be0cf234ee351495e892b20a8e0
engsr6982 <pwxg286704@163.com>
2025-07-19 15:16:56 +0800

    feat(gui): add EditLandPermTableUtilGUI for land permission editing

--- src/pland/gui/LandManagerGUI.h
+++ src/pland/gui/LandManagerGUI.h
@@ -20,16 +20,16 @@ public:
     LDAPI static void _implRemoveParentLandGUI(Player& player, SharedLand const& ptr);            // 删除父领地
     LDAPI static void _implRemoveMixLandGUI(Player& player, SharedLand const& ptr);               // 删除混合领地
 
-    LDAPI static void sendEditLandNameGUI(Player& player, SharedLand const& ptr);   // 编辑领地名称
-    LDAPI static void sendEditLandDescGUI(Player& player, SharedLand const& ptr);       // 编辑领地描述
-    LDAPI static void sendTransferLandGUI(Player& player, SharedLand const& ptr);       // 转让领地
+    LDAPI static void sendEditLandNameGUI(Player& player, SharedLand const& ptr);                 // 编辑领地名称
+    LDAPI static void sendEditLandDescGUI(Player& player, SharedLand const& ptr);                 // 编辑领地描述
+    LDAPI static void sendTransferLandGUI(Player& player, SharedLand const& ptr);                 // 转让领地
     LDAPI static void _sendTransferLandToOfflinePlayerGUI(Player& player, SharedLand const& ptr); // 转让领地给离线玩家
-    LDAPI static void sendChangLandRangeGUI(Player& player, SharedLand const& ptr);     // 更改领地范围
+    LDAPI static void sendChangLandRangeGUI(Player& player, SharedLand const& ptr);               // 更改领地范围
 
-    LDAPI static void sendChangeMemberGUI(Player& player, SharedLand ptr);                   // 更改成员
-    LDAPI static void _sendAddMemberGUI(Player& player, SharedLand ptr);                     // 添加在线成员
-    LDAPI static void _sendAddOfflineMemberGUI(Player& player, SharedLand ptr);              // 添加离线成员
-    LDAPI static void _sendRemoveMemberGUI(Player& player, SharedLand ptr, UUIDs members);   // 移除成员
+    LDAPI static void sendChangeMemberGUI(Player& player, SharedLand ptr);                 // 更改成员
+    LDAPI static void _sendAddMemberGUI(Player& player, SharedLand ptr);                   // 添加在线成员
+    LDAPI static void _sendAddOfflineMemberGUI(Player& player, SharedLand ptr);            // 添加离线成员
+    LDAPI static void _sendRemoveMemberGUI(Player& player, SharedLand ptr, UUIDs members); // 移除成员
 };
 
 
