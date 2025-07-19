6a54ab8e31bc733869c532ef25e3bd0f812ae86b
engsr6982 <pwxg286704@163.com>
2025-07-19 18:39:32 +0800

    fix(ChangeLandRangeSelector): forgot to initialize mLand in constructor

--- src/pland/selector/ChangeLandRangeSelector.cc
+++ src/pland/selector/ChangeLandRangeSelector.cc
@@ -11,7 +11,8 @@ namespace land {
 
 
 ChangeLandRangeSelector::ChangeLandRangeSelector(Player& player, SharedLand land)
-: ISelector(player, land->getDimensionId(), land->is3D()) {
+: ISelector(player, land->getDimensionId(), land->is3D()),
+  mLand(land) {
     mOldRangeDrawId = PLand::getInstance().getDrawHandleManager()->getOrCreateHandle(player)->draw(
         land->getAABB(),
         land->getDimensionId(),
