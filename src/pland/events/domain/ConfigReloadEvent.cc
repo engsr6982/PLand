7a718b6197587e62dc305e46309a03748a02fae9
engsr6982 <pwxg286704@163.com>
2026-08-07 14:11:10 +0800

    fix: move ConfigReloadEvent from events to event namespace

--- src/pland/events/domain/ConfigReloadEvent.cc
+++ src/pland/events/domain/ConfigReloadEvent.cc
@@ -1,8 +1,8 @@
 #include "ConfigReloadEvent.h"
 #include "pland/events/Helper.h"
 
-namespace land::events::inline infra {
+namespace land::event {
 
 IMPLEMENT_EVENT_EMITTER(ConfigReloadEvent)
 
-} // namespace land::events::inline infra
+} // namespace land::event
